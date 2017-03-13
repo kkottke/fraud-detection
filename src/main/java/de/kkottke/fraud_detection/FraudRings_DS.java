7b11d91e11a12f506373351e318a742b5ec800bc
Kristian Kottke <Kristian.Kottke@iteratec.de>
2017-03-13 22:34:54 +0100

    Refactored test data.

--- src/main/java/de/kkottke/fraud_detection/FraudRings_DS.java
+++ src/main/java/de/kkottke/fraud_detection/FraudRings_DS.java
@@ -10,6 +10,7 @@ import org.apache.flink.api.java.io.neo4j.Neo4jOutputFormat;
 import org.apache.flink.api.java.operators.DeltaIteration;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.util.Collector;
 
@@ -18,20 +19,20 @@ public class FraudRings_DS {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        DataSet<Tuple3<String, String, String>> input = env.readCsvFile("/Users/kkt/workspaces/fraud-detection/src/main/resources/input.txt")
-                .types(String.class, String.class, String.class);
+        DataSet<Tuple4<String, String, String, String>> input = env.readCsvFile("/Users/kkt/workspaces/fraud-detection/src/main/resources/input.txt")
+                .types(String.class, String.class, String.class, String.class);
         DataSet<Tuple3<String, String, String>> vertices = input.flatMap(new VertexExtractor()).distinct(2);
         DataSet<Tuple2<String, String>> edges = input.flatMap(new EdgeExtractor());
 
         // open a delta iteration
         DeltaIteration<Tuple3<String, String, String>, Tuple3<String, String, String>> iteration =
-                vertices.iterateDelta(vertices, 100, 2);
+                vertices.iterateDelta(vertices, 100, 0);
 
         // apply the step logic: join with the edges, select the minimum neighbor, update if the component of the candidate is smaller
         DataSet<Tuple3<String, String, String>> changes =
-                iteration.getWorkset().join(edges).where(2).equalTo(0).with(new MessageWithComponentIDJoin())
+                iteration.getWorkset().join(edges).where(0).equalTo(0).with(new MessageWithComponentIDJoin())
                 .groupBy(0).aggregate(Aggregations.MIN, 1)
-                .join(iteration.getSolutionSet()).where(0).equalTo(2)
+                .join(iteration.getSolutionSet()).where(0).equalTo(0)
                 .with(new ComponentIdFilter());
 
         // close the delta iteration (delta and new workset are identical)
@@ -41,11 +42,11 @@ public class FraudRings_DS {
                 iterationResult.filter(new FilterFunction<Tuple3<String, String, String>>() {
                     @Override
                     public boolean filter(Tuple3<String, String, String> value) throws Exception {
-                        return value.f1.equals("User");
+                        return value.f1.equals("user");
                     }
                 })
-                        .join(edges).where(2).equalTo(0)
-                        .join(iterationResult).where("f1.f1").equalTo(2).with(new VertexPair());
+                        .join(edges).where(0).equalTo(0)
+                        .join(iterationResult).where("f1.f1").equalTo(0).with(new VertexPair());
 //        result.print();
 
         writeToNeo4j(result);
@@ -56,32 +57,32 @@ public class FraudRings_DS {
     //     USER FUNCTIONS
     // *************************************************************************
 
-    public static final class VertexExtractor implements FlatMapFunction<Tuple3<String, String, String>, Tuple3<String, String, String>> {
+    public static final class VertexExtractor implements FlatMapFunction<Tuple4<String, String, String, String>, Tuple3<String, String, String>> {
 
         Tuple3<String, String, String> source = new Tuple3<>();
         Tuple3<String, String, String> target = new Tuple3<>();
 
         @Override
-        public void flatMap(Tuple3<String, String, String> line, Collector<Tuple3<String, String, String>> out) {
-            source.f0 = RandomStringUtils.randomAlphanumeric(10);
-            source.f1 = "User";
-            source.f2 = line.f0;
+        public void flatMap(Tuple4<String, String, String, String> line, Collector<Tuple3<String, String, String>> out) {
+            source.f0 = line.f0;
+            source.f1 = line.f1;
+            source.f2 = RandomStringUtils.randomAlphanumeric(10);
             out.collect(source);
 
-            target.f0 = RandomStringUtils.randomAlphanumeric(10);
-            target.f1 = line.f1;
-            target.f2 = line.f2;
+            target.f0 = line.f2;
+            target.f1 = line.f3;
+            target.f2 = RandomStringUtils.randomAlphanumeric(10);
             out.collect(target);
         }
     }
 
-    public static final class EdgeExtractor implements FlatMapFunction<Tuple3<String, String, String>, Tuple2<String, String>> {
+    public static final class EdgeExtractor implements FlatMapFunction<Tuple4<String, String, String, String>, Tuple2<String, String>> {
 
         Tuple2<String, String> edge1 = new Tuple2<>();
         Tuple2<String, String> edge2 = new Tuple2<>();
 
         @Override
-        public void flatMap(Tuple3<String, String, String> line, Collector<Tuple2<String, String>> out) {
+        public void flatMap(Tuple4<String, String, String, String> line, Collector<Tuple2<String, String>> out) {
             edge1.f0 = line.f0;
             edge1.f1 = line.f2;
             out.collect(edge1);
@@ -98,7 +99,7 @@ public class FraudRings_DS {
 
         @Override
         public Tuple2<String, String> join(Tuple3<String, String, String> vertex, Tuple2<String, String> edge) {
-            return new Tuple2<>(edge.f1, vertex.f0);
+            return new Tuple2<>(edge.f1, vertex.f2);
         }
     }
 
@@ -106,8 +107,8 @@ public class FraudRings_DS {
 
         @Override
         public void join(Tuple2<String, String> message, Tuple3<String, String, String> vertex, Collector<Tuple3<String, String, String>> out) {
-            if (message.f1.compareTo(vertex.f0) < 0) {
-                vertex.f0 = message.f1;
+            if (message.f1.compareTo(vertex.f2) < 0) {
+                vertex.f2 = message.f1;
                 out.collect(vertex);
             }
         }
@@ -142,12 +143,12 @@ public class FraudRings_DS {
                         "MERGE (user:User {name: i.name1, ringId: i.ringId1}) " +
                         "MERGE (prop:Property {name: i.name2, ringId: i.ringId2}) " +
                         "MERGE (user)-[:has]->(prop)")
-                .addParameterKey(0, "ringId1")
-                .addParameterKey(1, "type1")
-                .addParameterKey(2, "name1")
-                .addParameterKey(3, "ringId2")
-                .addParameterKey(4, "type2")
-                .addParameterKey(5, "name2")
+                .addParameterKey("name1")
+                .addParameterKey("type1")
+                .addParameterKey("ringId1")
+                .addParameterKey("name2")
+                .addParameterKey("type2")
+                .addParameterKey("ringId2")
                 .setTaskBatchSize(100).finish();
         result.output(outputFormat).setParallelism(1);
     }
