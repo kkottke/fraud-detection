95e7156ca29f26cb4905b1126d1fec2b8e58adb8
Kristian Kottke <Kristian.Kottke@iteratec.de>
2017-03-25 03:00:48 +0100

    Implemented neo4j file export.

--- src/main/java/de/kkottke/fraud_detection/FraudRingDetection_GSA.java
+++ src/main/java/de/kkottke/fraud_detection/FraudRingDetection_GSA.java
@@ -2,13 +2,13 @@ package de.kkottke.fraud_detection;
 
 import de.kkottke.fraud_detection.util.EdgeExtractor;
 import de.kkottke.fraud_detection.util.VertexExtractor;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.neo4j.Neo4jOutputFormat;
 import org.apache.flink.api.java.tuple.*;
-import org.apache.flink.graph.EdgeDirection;
-import org.apache.flink.graph.Graph;
-import org.apache.flink.graph.Triplet;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.graph.*;
 import org.apache.flink.graph.gsa.*;
 import org.apache.flink.types.NullValue;
 
@@ -17,7 +17,8 @@ public class FraudRingDetection_GSA {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        DataSet<Tuple2<String, String>> input = env.readCsvFile("/Users/kkt/workspaces/fraud-detection/src/main/resources/input.txt")
+        DataSet<Tuple2<String, String>> input = env.readCsvFile("/Users/kkt/Projekte/Flink/input/testdata.csv")
+//        DataSet<Tuple2<String, String>> input = env.readCsvFile("/Users/kkt/workspaces/fraud-detection/src/main/resources/input.txt")
                 .types(String.class, String.class);
         DataSet<Tuple2<String, String>> vertices = input.flatMap(new VertexExtractor()).distinct(0);
         DataSet<Tuple3<String, String, NullValue>> edges = input.flatMap(new EdgeExtractor(true));
@@ -29,9 +30,11 @@ public class FraudRingDetection_GSA {
         Graph<String, String, NullValue> resultGraph = graph.runGatherSumApplyIteration(
                 new GenerateLabel(), new ChooseMinLabel(), new UpdateLabel(), 100, config);
 
-        DataSet<Triplet<String, String, NullValue>> result = resultGraph.getTriplets();
-//        result.print();
-        writeToNeo4j(result.project(0, 1, 2, 3));
+        resultGraph.getVertices().map(new VertexResultMapper())
+                .writeAsCsv("/Users/kkt/Projekte/Flink/output/nodes.csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+        resultGraph.getEdges().map(new EdgeResultMapper())
+                .writeAsCsv("/Users/kkt/Projekte/Flink/output/edges.csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+//        printTriplets(resultGraph);
         env.execute();
     }
 
@@ -63,6 +66,39 @@ public class FraudRingDetection_GSA {
                 setResult(newValue);
             }
         }
+    }
+
+    public static final class VertexResultMapper implements MapFunction<Vertex<String, String>, Tuple3<String, String, String>> {
+
+        private Tuple3<String, String, String> tuple = new Tuple3<>();
+
+        @Override
+        public Tuple3<String, String, String> map(Vertex<String, String> vertex) throws Exception {
+            String[] idType = vertex.getId().split(":");
+            tuple.f0 = idType[0];
+            tuple.f1 = idType[1];
+            tuple.f2 = vertex.getValue();
+
+            return tuple;
+        }
+    }
+
+    public static final class EdgeResultMapper implements MapFunction<Edge<String, NullValue>, Tuple2<String, String>> {
+
+        private Tuple2<String, String> tuple = new Tuple2<>();
+
+        @Override
+        public Tuple2<String, String> map(Edge<String, NullValue> edge) throws Exception {
+            tuple.f0 = edge.getSource().split(":")[0];
+            tuple.f1 = edge.getTarget().split(":")[0];
+
+            return tuple;
+        }
+    }
+
+    private static void printTriplets(Graph<String, String, NullValue> graph) throws Exception {
+        DataSet<Triplet<String, String, NullValue>> result = graph.getTriplets();
+        result.print();
     }
 
     private static void writeToNeo4j(DataSet<Tuple4<String, String, String, String>> result) {
---
+++ src/main/java/de/kkottke/fraud_detection/examples/util/DataGenerator.java
@@ -0,0 +1,22 @@
+package de.kkottke.fraud_detection.examples.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataGenerator {
+
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("/Users/kkt/Projekte/Flink/input/testdata.csv");
+
+        for (int i = 0; i < 100000; i++) {
+            writer.write("user" + i + ":user,0162-" + i + ":handy\n");
+            writer.write("user" + i + ":user,street-" + i + ":post\n");
+            writer.write("user" + i + ":user,user-" + i + "@mail.com:mail\n");
+            writer.write("user" + i + ":user,DE-" + i + ":bank\n");
+            writer.write("user" + i + ":user," + i + ":card\n");
+        }
+
+        writer.flush();
+        writer.close();
+    }
+}
